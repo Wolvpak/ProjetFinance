d694d25d62c65674ded10a76d073f95b3480ad38
remicazorla <remikazor@gmail.com>
2022-12-01 18:07:46 +0100

    nettoyage fichiers + maj clustering

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import numpy as np
 import ta
 import yfinance as yf
 from ta import add_all_ta_features
-<<<<<<< HEAD
-=======
 import matplotlib as plt
->>>>>>> 67ae792bb6c174824e77ff6aeff932cf3eeab747
+
 
 #saves a dictionnary containing the financials and metrics from every company, for every available year
 def download_stocks_fundamentals(stocks_list, filename, api_key):
@@ -32,13 +30,11 @@ def db_fundamentals_summary(stock_list, database, year = '2021'):
             None
     return yearly_data.transpose()
 
-
-#librarie qui permet de calculer tous les indicateurs une fois le fichier recupéré
+#adds TA to price dataframe from yfinance
 def add_classic_indicators(df):
     np.seterr(invalid='ignore')
     df['adx 4'] = ta.trend.adx(high=df['High'], low=df['Low'], close = df['Close'], window = 4)
     
-#moyennes mobiles
     # EMA
     df['ema7']=ta.trend.ema_indicator(close=df['Close'], window=7)
     df['ema30']=ta.trend.ema_indicator(close=df['Close'], window=30)
@@ -55,8 +51,6 @@ def add_classic_indicators(df):
     df['sma150']=ta.trend.sma_indicator(close=df['Close'], window=150)
     df['sma200']=ta.trend.sma_indicator(close=df['Close'], window=200)
     
-    
-#oscillateurs
     # MACD
     macd2452 = ta.trend.MACD(close=df['Close'], window_fast=24, window_slow=52, window_sign=9)
     df['macd_24_52'] = macd2452.macd_diff()
@@ -123,15 +117,7 @@ def add_classic_indicators(df):
     #add talib features (volume, trend, momentum, volatility)
     ta_df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume")
     ta_df.index = ta_df.index.date
-    
     return pd.concat([df,ta_df],axis=1).T.drop_duplicates().T.iloc[200:,:]
-
-
-
-
-#comprendre comment evaluer la qualité de variables (forward stock return)
-#https://github.com/stefan-jansen/machine-learning-for-trading/blob/main/07_linear_models/05_predicting_stock_returns_with_linear_regression.ipynb
-#https://github.com/stefan-jansen/machine-learning-for-trading/blob/main/07_linear_models/04_statistical_inference_of_stock_returns_with_statsmodels.ipynb
 
 def get_euronext_tickers():
     df = pd.read_csv('euronext_tickers.csv', encoding = 'unicode_escape')
@@ -158,9 +144,8 @@ def generate_lagged_variables(df):
     #peut se faire avec toutes les variables mais a eviter avec lstm et DRL peut etre
     return df
 
-#mean reversion
+
 #https://admiralmarkets.com/education/articles/forex-indicators/macd-indicator-in-depth#:~:text=of%20future%20performance.-,MACD%20Indicator%20Settings%20for%20Intraday%20Trading,that%20works%20well%20on%20M30.
-#et d'autres
 def add_strategy_signals(df):
     return df
 
@@ -172,26 +157,27 @@ def add_fin_ratios_and_commodities(df): #FAMA, Beta, Omega, Sortino, Calmar
     return df
 
    
-def get_market_data():
-    Movements, Open, High, Low, Close = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
+def get_market_data(mean_volume):
+    Open, High, Low, Close = pd.DataFrame(), pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
     tickers = get_euronext_tickers()
     for ticker in tickers:
         try:
             stock_data = get_stock_data(ticker)
-            Movements[ticker] = stock_data['Close']-stock_data['Open']
-            Open[ticker] = stock_data['Open']
-            High[ticker] = stock_data['High']
-            Low[ticker] = stock_data['Low']
-            Close[ticker] = stock_data['Close']
+            #print(ticker, stock_data['Volume'].mean())
+            if stock_data['Volume'].mean()> mean_volume:
+                Open[ticker] = stock_data['Open']
+                High[ticker] = stock_data['High']
+                Low[ticker] = stock_data['Low']
+                Close[ticker] = stock_data['Close']
         except:
-            None
-    Movements = Movements.loc[:,Movements.isnull().sum() < 1]     
+            None   
     Open = Open.loc[:,Open.isnull().sum() < 1]   
     High = High.loc[:,High.isnull().sum() < 1]  
     Low = Low.loc[:,Low.isnull().sum() < 1]  
     Close = Close.loc[:,Close.isnull().sum() < 1]        
-    print(f'{len(Movements.columns)*100/len(tickers)} % of tickers are available')
-    return Movements, Open, High, Low, Close
+    print(f'{len(Close.columns)*100/len(tickers)} % of tickers are available')
+    return Open, High, Low, Close
             
-    
+#https://towardsdatascience.com/implementation-of-technical-indicators-into-a-machine-learning-framework-for-quantitative-trading-44a05be8e06
+
 #reste : VIX, supertrend, analyse de sentiment(BERT), et strategies (vol anomaly), varmax
